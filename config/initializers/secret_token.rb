df0b74f2cc52449f0720cc28d2e4f2093283dae6
Danielle <mdlee724@gmail.com>
2014-02-24 17:48:37 -0800

    add a bare app to track changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrossfitMngmt::Application.config.secret_key_base = '497d57b7cce1f7f427d7f42d2797d2da5cb0653038929fdc565e0b61ddcf89a0b915f83b3b4f08fd65a892f66a6ebc6a6541d32331dbc6cd2c763fd650739cda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrossfitMngmt::Application.config.session_store :cookie_store, key: '_crossfit_mngmt_session'
